6ce94be39fa697162964a7855936b72d98b2d1e3
Fahri MZ <mzfahri620@gmail.com>
2018-11-18 18:51:54 +0700

    Add fixed sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
+
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+} from '@angular/material';
+
 import { environment } from '../environments/environment';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
@@ -21,7 +27,9 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })  
+    MatSidenavModule,
+    MatIconModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
